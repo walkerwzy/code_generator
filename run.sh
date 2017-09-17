d83e12ca30b2ad055838ca06a1e44f709cab1d44
walker <walker.wzy@gmail.com>
2017-09-18 01:44:56 +0800

    更新支持task后的脚本

--- run.sh
+++ run.sh
@@ -2,46 +2,28 @@
 author='walker'
 projectname='Homedoctor'
 passkeys='code,flag,message'
-fileuri='index.html'
+fileuri='/Users/walker/Desktop/index.html'
 debug=true
 verbose=false
 models=(
-#获取扫码后待操作的订单
-'modelPendingOrderListResponse'
-#订单列表
-'modelOrderListResponse,modelOrderListDetail'
-#服务包退订
-'modelServicePackageTDResponse'
-#去支付（修改订单状态为待支付）
-'modelOrderToPayResponse'
-#线下支付
-'modelOfflinePayResonse'
-#订单拒绝
-'modelOrderRefuseResponse'
-#订单确认
-'modelOrderConfirmResponse'
-#订单取消
-'modelOrderCancelResponse'
-#生成签约服务订单
-'modelOrderQianyueResponse'
-#获取配置项
-'modeOrdereConfigResponse,modelOrderConfigInfo,modelOrderConfigShowItem,modelOrderConfigRequireItem'
-#保存草稿
-'modelOrderSaveResponse,modelAgreementTagsInfo'
-#修改草稿
-'modelOrderModifyResponse,modelAgreementTagsInfo'
-#草稿详情
-'modelAgreementOrderDetailResponse,modelAgreemntOrderDetailInfo,modelAgreementAttatch,modelAgreementTagsInfo'
-#订单校验
-'modelOrderApplyResponse'
-#订单支付
-'modelOrderSubmitResponse,modelOrderSubmitInfo'
-#订单详情
-'modelOrderDetailResponse,modelOrderDetailInfo,modelOrderDetailImgInfo,modelOrderDetailPkgItem'
-#服务包详情
-'modelServicePackageResponse,modelServicePackageInfo,modelServicePackageDO'
-#服务包列表（签约个人和签约团队的所有服务包）
-'modelServicePkgListResponse,modelServicePackageInfo'
+'获取扫码后待操作的订单:modelPendingOrderListResponse'
+'订单列表:modelOrderListResponse,modelOrderListDetail'
+'服务包退订:modelServicePackageTDResponse'
+'去支付（修改订单状态为待支付）:modelOrderToPayResponse'
+'线下支付:modelOfflinePayResonse'
+'订单拒绝:modelOrderRefuseResponse'
+'订单确认:modelOrderConfirmResponse'
+'订单取消:modelOrderCancelResponse'
+'生成签约服务订单:modelOrderQianyueResponse'
+'获取配置项:modeOrdereConfigResponse,modelOrderConfigInfo,modelOrderConfigShowItem,modelOrderConfigRequireItem'
+'保存草稿:modelOrderSaveResponse,modelAgreementTagsInfo'
+'修改草稿:modelOrderModifyResponse,modelAgreementTagsInfo'
+'草稿详情:modelAgreementOrderDetailResponse,modelAgreemntOrderDetailInfo,modelAgreementAttatch,modelAgreementTagsInfo'
+'订单校验:modelOrderApplyResponse'
+'订单支付:modelOrderSubmitResponse,modelOrderSubmitInfo'
+'订单详情:modelOrderDetailResponse,modelOrderDetailInfo,modelOrderDetailImgInfo,modelOrderDetailPkgItem'
+'服务包详情:modelServicePackageResponse,modelServicePackageInfo,modelServicePackageDO'
+'服务包列表（签约个人和签约团队的所有服务包）:modelServicePkgListResponse,modelServicePackageInfo'
 )
 datakeys=(
 'infoIsshow'
@@ -53,17 +35,24 @@ datakeys=(
 'imgUrls'
 )
 
-for name in ${models[@]}; do
-	types=${types}${name},
-done;
-types=${types}cEOF
+for model in ${models[@]}; do
+	desp=${desp}${model%%:*},
+	types=${types}${model#*:},
+done
+types=${types}
+desp=${desp}
+
+# for name in ${models[@]}; do
+# 	types=${types}${name},
+# done;
+# types=${types}
 #如果 types 比较简单, 请直接覆盖types变量
 
 for key in ${datakeys[@]}; do
 	keys=${keys}${key},
 done
-keys=${keys}kEOF
-#如果 datakey 比较简单, 请直接覆盖keys变量
+keys=${keys}
+# 如果 datakey 比较简单, 请直接覆盖keys变量
 
 if [ "$debug" = true ] ; then
 	enableDebug='--debug'
@@ -75,8 +64,10 @@ fi
 
 ./app.js ${enableVerbose} ${enableDebug} \
     -C ${types} \
+    -D ${desp} \
 	-K ${keys} \
     -P ${passkeys} \
     -f ${fileuri} \
     -a ${author} \
+    -m ${modulename} \
     -j ${projectname}
--- scripts/10.签约随访.sh
+++ scripts/10.签约随访.sh
@@ -6,33 +6,35 @@ fileuri='/Users/walker/Desktop/index.html'
 debug=true
 verbose=false
 models=(
-# 随访模板列表
-'modelFollowTemplateListResponse,modelFollowTemplateListItem'
-# 随访模板详情
-'modelFollowTemplateDetailResponse,modelFollowTemplateDetail,modelFollowTemplateDetailField,modelFollowTemplateFieldOption'
-# 随访记录列表
-'modelFollowUpListResponse,modelFollowUpListItem'
-# 随访评价
-'modelFollowEvaluateResponse'
-# 随访详情
-'modelFollowUpDetailResponse,modelFollowUpDetailInfo,modelFollowUpDetailField'
+'随访模板列表:modelFollowTemplateListResponse,modelFollowTemplateListItem'
+'随访模板详情:modelFollowTemplateDetailResponse,modelFollowTemplateDetail,modelFollowTemplateDetailField,modelFollowTemplateFieldOption'
+'随访记录列表:modelFollowUpListResponse,modelFollowUpListItem'
+'随访评价:modelFollowEvaluateResponse'
+'随访详情:modelFollowUpDetailResponse,modelFollowUpDetailInfo,modelFollowUpDetailField'
 )
 datakeys=(
 'fields'
 'options'
 )
 
-for name in ${models[@]}; do
-	types=${types}${name},
-done;
-types=${types}cEOF
+for model in ${models[@]}; do
+	desp=${desp}${model%%:*},
+	types=${types}${model#*:},
+done
+types=${types}
+desp=${desp}
+
+# for name in ${models[@]}; do
+# 	types=${types}${name},
+# done;
+# types=${types}
 #如果 types 比较简单, 请直接覆盖types变量
 
 for key in ${datakeys[@]}; do
 	keys=${keys}${key},
 done
-keys=${keys}kEOF
-#如果 datakey 比较简单, 请直接覆盖keys变量
+keys=${keys}
+# 如果 datakey 比较简单, 请直接覆盖keys变量
 
 if [ "$debug" = true ] ; then
 	enableDebug='--debug'
@@ -44,8 +46,10 @@ fi
 
 ./app.js ${enableVerbose} ${enableDebug} \
     -C ${types} \
+    -D ${desp} \
 	-K ${keys} \
     -P ${passkeys} \
     -f ${fileuri} \
     -a ${author} \
+    -m ${modulename} \
     -j ${projectname}
