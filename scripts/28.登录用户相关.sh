7266c36bb471a99d7ace0f4253d416281bd9eac5
walker <walker.wzy@gmail.com>
2017-09-19 17:04:19 +0800

    添加脚本

---
+++ scripts/28.登录用户相关.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+author='walker'
+projectname='Homedoctor'
+passkeys='code,flag,message'
+fileuri='/Users/walker/Desktop/index.html'
+debug=true
+verbose=false
+modulename='taskUserManager'
+models=(
+#退出登录
+#编辑头像
+#个人信息
+'modelUserProfile'
+)
+
+for model in ${models[@]}; do
+    types=${types}${model},
+done
+types=${types}
+
+if [ "$debug" = true ] ; then
+    enableDebug='--debug'
+fi
+
+if [ "$verbose" = true ] ; then
+    enableVerbose='--verbose'
+fi
+
+./app.js ${enableVerbose} ${enableDebug} \
+    -C ${types} \
+    -P ${passkeys} \
+    -f ${fileuri} \
+    -a ${author} \
+    -m ${modulename} \
+    -j ${projectname}
