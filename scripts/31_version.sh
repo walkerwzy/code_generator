6aa0eeec6ce93eb2b907e9a942a581357a7c3190
Evan <boaosady@hotmail.com>
2017-09-21 15:17:59 +0800

    删除本地版本

--- scripts/31_version.sh
+++
@@ -1,9 +0,0 @@
-./app.js \
-    -C modelVersionInfoResponse,modelVersionInfo,nouse \
-    -P code,flag,message \
-    -f /Users/Evan/Desktop/index.html \
-    -a Evan \
-    -j HomeDoctor \
-    -d true  \
-    -- verbose
-
