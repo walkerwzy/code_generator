7292fb498b44e08a369218806b3b2814cf7bacd5
walker <walker.wzy@gmail.com>
2018-01-03 11:28:34 +0800

    增加医生版目录

---
+++ scripts/jyd/2.用户相关.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+author='walker'
+projectname='FamilyDoctor'
+passkeys='code,flag,message'
+fileuri='/Users/walker/Desktop/index.html'
+debug=true
+verbose=false
+modulename='taskUserInfo'
+models=(
+#用户登录
+'modelUserHospital'
+#修改用户密码
+#找回密码
+#微信免登陆
+'modelUesrWechat'
+#退出登录
+#个人信息
+'modelUserProfile'
+#更改医生个人信息
+#获取签约患者的头像和姓名
+'modelUserBaseInfo'
+)
+
+for model in ${models[@]}; do
+    types=${types}${model},
+done
+types=${types}
+
+if [ "$debug" = true ] ; then
+    enableDebug='--debug'
+fi
+
+if [ "$verbose" = true ] ; then
+    enableVerbose='--verbose'
+fi
+
+./app.js ${enableVerbose} ${enableDebug} \
+    -C ${types} \
+    -P ${passkeys} \
+    -f ${fileuri} \
+    -a ${author} \
+    -m ${modulename} \
+    -j ${projectname}
