6fcbdda27fbed84edc882bfcd3fe18f2313ba965
maweefeng <maweefeng@gmail.com>
2018-01-24 14:33:44 +0800

    5.签约相关

---
+++ scripts/jyd/5.签约相关.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+author='maweefeng'
+projectname='FamilyDoctor'
+passkeys='code,flag,message'
+fileuri='/Users/maweefeng/Desktop/index.html'
+offset=1
+debug=true
+verbose=false
+modulename='taskAgreement'
+httpclient='PMLRESTBaseDoctor'
+models=(
+#签约关系列表
+'modelAgreeDataList','modelAgreeDataListItem','modelAgreeServicePkgsItem'
+#签约居民基础信息(只获取展示的有效字段)
+'modelAgreementCitizenItem'
+
+#签约关系详情
+'modelAgreeDetailList','modelAgreementServicePkgsItem','modelServicePkgOrderDTOListItem','modelAgreementTagsListItem','modeltxWdInfoListItem'
+#修改签约居民信息
+'modelUUIDList'
+
+)
+
+for model in ${models[@]}; do
+    types=${types}${model},
+done
+types=${types}
+
+if [ "$debug" = true ] ; then
+    enableDebug='--debug'
+fi
+
+if [ "$verbose" = true ] ; then
+    enableVerbose='--verbose'
+fi
+
+./app.js ${enableVerbose} ${enableDebug} \
+    -C ${types} \
+    -P ${passkeys} \
+    -f ${fileuri} \
+    -a ${author} \
+    -m ${modulename} \
+    -j ${projectname} \
+    -B ${httpclient} \
+    -o ${offset}
