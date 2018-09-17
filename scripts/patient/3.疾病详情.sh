067d80885033bcc9020d3c77e9af4f7f3e08c828
walker <walker.wzy@gmail.com>
2018-09-17 18:42:55 +0800

    ad

---
+++ scripts/patient/3.疾病详情.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+author='walker'
+projectname='WYMTCPaitentModule'
+passkeys=''
+fileuri='/Users/walker/Desktop/index.html'
+offset=1
+debug=true
+verbose=false
+prefix='WYMTCPT'
+modulename='taskDoctorInfo'
+httpclient='NSObject'
+models=(
+#医生详情
+'ConsultDetailListItem,ConsultDrugListItem,ConsultInfoDO'
+)
+
+for model in ${models[@]}; do
+    types=${types}${model},
+done
+types=${types}
+
+if [ "$debug" = true ] ; then
+    enableDebug='--debug'
+fi
+
+if [ "$verbose" = true ] ; then
+    enableVerbose='--verbose'
+fi
+
+./app.js ${enableVerbose} ${enableDebug} \
+    -C ${types} \
+    -P ${passkeys} \
+    -f ${fileuri} \
+    -a ${author} \
+    -m ${modulename} \
+    -j ${projectname} \
+    -x ${prefix} \
+    -B ${httpclient} \
+    -o ${offset}
